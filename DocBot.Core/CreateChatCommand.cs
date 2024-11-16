e4012f9adab64699f4ad38eececcf2ba5e096f84
aargarcia <aargarcia@aidacanarias.com>
2024-11-16 23:32:22 +0000

    WIP: create chat command handler

--- DocBot.Core/CreateChatCommand.cs
+++ DocBot.Core/CreateChatCommand.cs
@@ -8,7 +8,28 @@ public class CreateChatCommand : IRequest<ChatResponse> {
 }
 
 public class CreateChatCommandHandler : IRequestHandler<CreateChatCommand, ChatResponse> {
+    private readonly ChatRepository chatRepository;
+
+    public CreateChatCommandHandler(ChatRepository chatRepository) {
+        this.chatRepository = chatRepository;
+    }
     public async Task<ChatResponse> Handle(CreateChatCommand request, CancellationToken cancellationToken) {
         return new ChatResponse{Guid = Guid.NewGuid().ToString()};
     }
+}
+
+public class ChatRepository {
+    public IList<Chat> Chats { get; } = new List<Chat>();
+    public void Update() {
+        
+    }
+
+    public Chat Get(Guid chatId) {
+        return new Chat();
+    }
+}
+
+public class Chat {
+    public Guid Guid { get; set; }
+    public string Name { get; set; }
 }
